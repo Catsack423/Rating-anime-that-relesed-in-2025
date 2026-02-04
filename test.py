787b2029945aee1b85c594a5e9fde50ddadb600a
Catsack423 <face11773@gmail.com>
2026-02-04 22:19:09 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 import requests
 import json
-
+import time
 
 
 res= requests.get("http://110.164.203.137:9919/anime")
@@ -10,7 +10,8 @@ res= requests.get("http://110.164.203.137:9919/anime")
 datas = res.json()
 cout =0
 for anime in datas :
-   print(anime.get("title"))
+    time.sleep(1)
+    print(anime.get("title"))
 
 
 
